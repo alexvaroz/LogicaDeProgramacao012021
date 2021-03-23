1c61fbcc44cc92652277e26af20a6de8a6beac7f
Alexandre <alexandre.vaz@gmail.com>
2021-03-23 11:04:59 -0300

    Fim da aula

---
+++ aula2303/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PrimeiroProjeto.Personagens;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!!!");
+            Console.WriteLine("Tenham todos um bom dia!!!!");
+            Console.WriteLine(1 + 2);
+            Console.WriteLine("a" + "b");
+            var a = 1;
+            var b = 2;
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+            Soldado soldado1 = new Soldado();
+        }
+    }
+}
