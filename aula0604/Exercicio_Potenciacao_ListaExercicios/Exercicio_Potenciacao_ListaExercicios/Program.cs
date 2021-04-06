6fd3812fc5778aaa3ca98dda519e0cc087ce4c78
Alexandre <alexandre.vaz@gmail.com>
2021-04-06 11:13:23 -0300

    Exercicio de potenciacao da lista 2.

---
+++ aula0604/Exercicio_Potenciacao_ListaExercicios/Exercicio_Potenciacao_ListaExercicios/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Potenciacao_ListaExercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero_base, numero_expoente;
+            int resultado = 1;
+            Console.WriteLine("Digite o número da base:");
+            numero_base = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o número do expoente:");
+            numero_expoente = int.Parse(Console.ReadLine());
+
+            if (numero_expoente > 0)
+            {
+                for (int i = 1; i <= numero_expoente; i++)
+                {
+                    resultado = resultado * numero_base;
+                }
+            }
+            Console.WriteLine(resultado);
+            Console.ReadLine();
+        }
+    }
+}
