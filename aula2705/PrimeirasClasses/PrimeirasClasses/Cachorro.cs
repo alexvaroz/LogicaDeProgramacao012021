0f1339f1b4958a96b54d85cfe1344cbc3b12f479
Alexandre <alexandre.vaz@gmail.com>
2021-05-28 06:32:33 -0300

    Aula concluida

---
+++ aula2705/PrimeirasClasses/PrimeirasClasses/Cachorro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeirasClasses
+{
+    class Cachorro
+    {
+        // Atributos
+        private string nome;
+        public string raca;
+        public string genero;
+        public int idade;
+        public bool estaLatindo = false;
+
+        // Métodos
+        public Cachorro() { }
+
+        public Cachorro(string nome_)
+        {
+            nome = nome_;
+        }
+        
+        
+        public void Latir()
+        {
+            estaLatindo = true;
+            Console.WriteLine($"{nome} latindo:");
+            Console.WriteLine("Au au au au");
+        }
+
+        public void AbanarRabinho()
+        {
+            Console.WriteLine("Vrap vrap vrap");
+        }
+    }
+}
---
+++ aula2705/PrimeirasClasses/PrimeirasClasses/ClasseTeste.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeirasClasses
+{
+    class ClasseTeste
+    {
+        static void Main(string[] args)
+        {
+            Cachorro fred = new Cachorro();
+            fred.nome = "Fred Mercurio";
+            fred.raca = "Chiuaua";
+            fred.genero = "macho";
+            fred.idade = 2;
+            
+            Cachorro billy = new Cachorro();
+            billy.nome = "Billy Boy";
+            billy.raca = "Fila";
+            billy.genero = "macho";
+            billy.idade = 1;
+           
+            Cachorro tobias = new Cachorro();            
+            tobias.nome = "Tobias Lopes";
+            tobias.raca = "PitBull";
+            tobias.genero = "macho";
+            tobias.idade = 5;
+
+            Cachorro lulu = new Cachorro("Lulu Linda");
+
+            Cachorro[] canil = { tobias, fred, billy, lulu };
+
+            foreach ( Cachorro cachorro in canil)
+            {
+                cachorro.Latir();
+            }
+
+            foreach (Cachorro cachorro in canil)
+            {
+                cachorro.idade++;
+            }
+
+            foreach (Cachorro cachorro in canil)
+            {
+                Console.WriteLine($"{cachorro.nome} tem {cachorro.idade} anos.");
+                cachorro.AbanarRabinho();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
